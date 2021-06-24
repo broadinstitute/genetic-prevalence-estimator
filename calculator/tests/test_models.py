d6cac154e8fb1acb9c8b9b5f98da15331bca1597
Nick Watts <nwatts@broadinstitute.org>
2021-06-24 08:44:32 -0400

    Add tests for unique UUIDs

--- calculator/tests/test_models.py
+++ calculator/tests/test_models.py
@@ -1,4 +1,6 @@
 # pylint: disable=no-self-use
+import uuid
+
 import pytest
 from django.contrib.auth import get_user_model
 from django.db import IntegrityError
@@ -10,6 +12,57 @@ User = get_user_model()
 
 
 class TestVariantList:
+    @pytest.mark.django_db
+    def test_uuid_is_unique(self):
+        uid = uuid.uuid4()
+
+        VariantList.objects.create(
+            uuid=uid,
+            id=1,
+            label="List 1",
+            type=VariantList.Type.CUSTOM,
+            metadata={"version": "1", "reference_genome": "GRCh37"},
+            variants=["1-55516888-G-GA"],
+        )
+
+        with pytest.raises(IntegrityError):
+            VariantList.objects.create(
+                uuid=uid,
+                id=2,
+                label="List 2",
+                type=VariantList.Type.CUSTOM,
+                metadata={"version": "1", "reference_genome": "GRCh37"},
+                variants=["1-55516888-G-GA"],
+            )
+
+
+class TestVariantListAccess:
+    @pytest.mark.django_db
+    def test_uuid_is_unique(self):
+        user1 = User.objects.create(username="user1")
+        user2 = User.objects.create(username="user2")
+
+        list1 = VariantList.objects.create(
+            id=1,
+            label="List 1",
+            type=VariantList.Type.CUSTOM,
+            metadata={"version": "1", "reference_genome": "GRCh37"},
+            variants=["1-55516888-G-GA"],
+        )
+
+        list2 = VariantList.objects.create(
+            id=2,
+            label="List 2",
+            type=VariantList.Type.CUSTOM,
+            metadata={"version": "1", "reference_genome": "GRCh37"},
+            variants=["1-55516888-G-GA"],
+        )
+
+        uid = uuid.uuid4()
+        VariantListAccess.objects.create(uuid=uid, user=user1, variant_list=list1)
+        with pytest.raises(IntegrityError):
+            VariantListAccess.objects.create(uuid=uid, user=user2, variant_list=list2)
+
     @pytest.mark.django_db
     def test_deleting_variant_list_deletes_permission_models(self):
         viewer = User.objects.create(username="viewer")
