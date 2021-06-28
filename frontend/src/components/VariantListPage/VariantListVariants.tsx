17d52dcd4708de4a99a7f909441c125a961bb53d
Nick Watts <nwatts@broadinstitute.org>
2021-06-28 14:58:25 -0400

    Link variants in variant list to gnomAD

--- frontend/src/components/VariantListPage/VariantListVariants.tsx
+++ frontend/src/components/VariantListPage/VariantListVariants.tsx
@@ -1,6 +1,18 @@
-import { ListItem, Text, UnorderedList } from "@chakra-ui/react";
+import { Link, ListItem, Text, UnorderedList } from "@chakra-ui/react";
 
 import { VariantList } from "../../types";
+
+const getReferenceGenomeForVariantList = (variantList: VariantList) => {
+  if (variantList.type === "custom") {
+    return variantList.metadata.reference_genome;
+  }
+
+  if (variantList.type === "gnomad") {
+    const gnomadVersion = variantList.metadata.gnomad_version;
+    const majorVersion = gnomadVersion.split(".")[0];
+    return majorVersion === "2" ? "GRCh37" : "GRCh38";
+  }
+};
 
 const VariantListVariants = (props: { variantList: VariantList }) => {
   const { variantList } = props;
@@ -21,10 +33,22 @@ const VariantListVariants = (props: { variantList: VariantList }) => {
     return <Text mb={4}>This variant list has no variants.</Text>;
   }
 
+  const referenceGenome = getReferenceGenomeForVariantList(variantList);
+  const gnomadDataset =
+    referenceGenome === "GRCh37" ? "gnomad_r2_1" : "gnomad_r3";
+
   return (
     <UnorderedList mb={4}>
       {variantList.variants.map((variantId) => (
-        <ListItem key={variantId}>{variantId}</ListItem>
+        <ListItem key={variantId}>
+          <Link
+            href={`https://gnomad.broadinstitute.org/variant/${variantId}?dataset=${gnomadDataset}`}
+            isExternal
+            target="_blank"
+          >
+            {variantId}
+          </Link>
+        </ListItem>
       ))}
     </UnorderedList>
   );
