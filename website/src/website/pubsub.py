a13bf8f36f0353c37a9ff8275f75000932406ad5
Nick Watts <nwatts@broadinstitute.org>
2021-06-28 08:41:40 -0400

    Lazily create Pub/Sub client

--- website/src/website/pubsub.py
+++ website/src/website/pubsub.py
@@ -4,13 +4,22 @@ from django.conf import settings
 from google.cloud import pubsub_v1
 
 
-publisher = pubsub_v1.PublisherClient()
-topic_path = publisher.topic_path(  # pylint: disable=no-member
-    settings.GCP_PROJECT, "worker-requests"
-)
+class Publisher:
+    client = None
+    topic_path = None
+
+    def send_to_worker(self, message, timeout=30):
+        if not self.client:
+            self.client = pubsub_v1.PublisherClient()
+
+        if not self.topic_path:
+            self.topic_path = self.client.topic_path(  # pylint: disable=no-member
+                settings.GCP_PROJECT, "worker-requests"
+            )
+
+        encoded_message = json.dumps(message).encode("utf-8")
+        future = self.client.publish(self.topic_path, encoded_message)
+        return future.result(timeout)
 
 
-def send_to_worker(message, timeout=30):
-    encoded_message = json.dumps(message).encode("utf-8")
-    future = publisher.publish(topic_path, encoded_message)
-    return future.result(timeout)
+publisher = Publisher()
