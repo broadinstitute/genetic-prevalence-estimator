803e48aa724b3342fe6b55cbe4053d5a57dcacf9
Nick Watts <nwatts@broadinstitute.org>
2021-09-10 08:40:25 -0400

    Correct prevalence calculation
    Resolves #58

--- frontend/src/components/VariantListPage/VariantListCalculations/calculations.ts
+++ frontend/src/components/VariantListPage/VariantListCalculations/calculations.ts
@@ -4,26 +4,23 @@ export const calculateCarrierFrequencyAndPrevalence = (
   variants: Variant[],
   variantList: VariantList
 ) => {
-  const zeros = [0, ...variantList.metadata.populations!.map(() => 0)];
-
-  const alleleFrequencies = variants.map((variant) => {
+  const variantAlleleFrequencies = variants.map((variant) => {
     return variant.AC!.map((ac, i) => {
       const an = variant.AN![i];
       return an === 0 ? 0 : ac / an;
     });
   });
 
-  const carrierFrequency = alleleFrequencies
-    .map((af) => af.map((q) => 2 * (1 - q) * q))
-    .reduce((acc, values) => acc.map((n, i) => n + values[i]), zeros);
+  const totalAlleleFrequencies = variantAlleleFrequencies.reduce(
+    (acc, values) => acc.map((q, i) => q + values[i]),
+    [0, ...variantList.metadata.populations!.map(() => 0)]
+  );
 
-  const carrierFrequencySimplified = alleleFrequencies
-    .map((af) => af.map((q) => 2 * q))
-    .reduce((acc, values) => acc.map((n, i) => n + values[i]), zeros);
+  const carrierFrequency = totalAlleleFrequencies.map((q) => 2 * (1 - q) * q);
 
-  const prevalence = alleleFrequencies
-    .map((af) => af.map((q) => q ** 2))
-    .reduce((acc, values) => acc.map((n, i) => n + values[i]), zeros);
+  const carrierFrequencySimplified = totalAlleleFrequencies.map((q) => 2 * q);
+
+  const prevalence = totalAlleleFrequencies.map((q) => q ** 2);
 
   return { carrierFrequency, carrierFrequencySimplified, prevalence };
 };
