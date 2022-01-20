eecca965d87452d815c69180fd639bd609a2fe5c
Nick Watts <nwatts@broadinstitute.org>
2022-01-20 12:08:22 -0500

    Label data bucket with name

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,4 +24,8 @@ resource "google_storage_bucket" "data_bucket" {
   name                        = "${var.gcp_project}-data"
   location                    = var.gcp_region
   uniform_bucket_level_access = true
+
+  labels = {
+    bucket : "${var.gcp_project}-data"
+  }
 }
