8cfd46d0cf337621f693234c25e0c8efeb944c61
Nick Watts <nwatts@broadinstitute.org>
2022-02-26 20:10:29 -0500

    Use DRF error handlers for worker

--- worker/src/worker/urls.py
+++ worker/src/worker/urls.py
@@ -4,3 +4,6 @@ from worker.views import receive_message_view
 
 
 urlpatterns = [path("", receive_message_view, name="receive-message")]
+
+handler400 = "rest_framework.exceptions.bad_request"  # pylint: disable=invalid-name
+handler500 = "rest_framework.exceptions.server_error"  # pylint: disable=invalid-name
