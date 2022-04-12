96aa00a195e2d2fcd9d2cce4d332d7ea3d8a55f3
Nick Watts <nwatts@broadinstitute.org>
2022-04-11 22:38:43 -0400

    Ignore unused argument in Pylint

--- website/src/website/views/error_views.py
+++ website/src/website/views/error_views.py
@@ -2,5 +2,5 @@ from rest_framework.response import Response
 from rest_framework.status import HTTP_403_FORBIDDEN
 
 
-def csrf_failure(request, reason=""):
+def csrf_failure(request, reason=""):  # pylint: disable=unused-argument
     return Response({"detail": "CSRF verification failed."}, status=HTTP_403_FORBIDDEN)
