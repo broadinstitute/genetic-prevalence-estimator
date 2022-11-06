8e2aa84bb2a190fdf104a021f3f49995085a5cad
Nick Watts <nwatts@broadinstitute.org>
2022-11-06 08:02:22 -0500

    Upgrade Terraform providers

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "4.6.0"
+      version = "4.42.1"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "4.6.0"
+      version = "4.42.1"
     }
   }
 
