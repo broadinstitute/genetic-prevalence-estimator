17a1e382e7a6d1b9fc04982ca689632cc3f5180e
Nick Watts <nwatts@broadinstitute.org>
2023-04-03 00:04:34 -0400

    Fix logging configuration

--- worker/src/worker/settings/production.py
+++ worker/src/worker/settings/production.py
@@ -10,11 +10,15 @@ cloud_logging_client = logging.Client()
 cloud_logging_client.setup_logging()
 
 LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
     "handlers": {
         "stackdriver": {
             "class": "google.cloud.logging.handlers.StructuredLogHandler",
             "client": cloud_logging_client,
         }
     },
-    "loggers": {"": {"handlers": ["stackdriver"], "level": "INFO"}},
+    "loggers": {
+        "root": {"handlers": ["stackdriver"], "level": "INFO"},
+    },
 }
