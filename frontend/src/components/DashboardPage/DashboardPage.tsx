7f7d8346469e98023c2b8ceeebf1c8f1732fd74a
Riley Grant <grantrileyh@gmail.com>
2023-05-15 17:21:51 -0500

    wip: Refactor Dashboard page component

--- frontend/src/components/DashboardPage/DashboardPage.tsx
+++ frontend/src/components/DashboardPage/DashboardPage.tsx
@@ -12,111 +12,159 @@ import {
   Heading,
   Link,
   Spinner,
-  Stat,
-  StatGroup,
-  StatLabel,
-  StatNumber,
   Table,
   Text,
   Thead,
   Tbody,
-  Tfoot,
   Tr,
   Th,
   Td,
-  TableCaption,
   TableContainer,
 } from "@chakra-ui/react";
-import { AddIcon, AttachmentIcon } from '@chakra-ui/icons'
-import { useEffect, useState } from "react";
+import { AttachmentIcon } from "@chakra-ui/icons";
+import { FC, useEffect, useState } from "react";
 import { Link as RRLink } from "react-router-dom";
 
-import { get } from "../../api";
-import { VariantListStatus } from "../../types";
-
 import DocumentTitle from "../DocumentTitle";
+import { get } from "../../api";
 
-const SupportingDocuments = ({document_exists}: any) => {
+const SupportingDocuments = ({ document_exists }: any) => {
   const [document, setDocument] = useState(document_exists);
-  
+
   return document ? (
-      <><h1> Supporting Documents</h1 >
-    <Button rightIcon={<AttachmentIcon />} colorScheme='teal' variant='outline'>
-      Supporting document
-    </Button></>
-
-    ) : (
-  <h1>No document</h1>
-);
-
-    
-  
-}
-
-const DashboardPage = () => {
-
-  return (
     <>
-      <DocumentTitle title="Prevalence lists" />
-
-      <Box mb={2}>
-        <Breadcrumb>
-          <BreadcrumbItem>
-            <BreadcrumbLink as={RRLink} to="/">
-              Home
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbItem isCurrentPage>
-            <span>Prevalence Lists</span>
-          </BreadcrumbItem>
-        </Breadcrumb>
-      </Box>
-
-      <DashboardContainer/>
+      <Button
+        rightIcon={<AttachmentIcon />}
+        size="sm"
+        colorScheme="teal"
+        variant="outline"
+      >
+        Supporting document
+      </Button>
     </>
+  ) : (
+    <Text>No document</Text>
   );
+};
 
+interface DashboardListProps {
+  dashboardList: any[];
 }
 
+const DashboardList: FC<DashboardListProps> = ({ dashboardList }) => {
+  return (
+    <TableContainer>
+      <Table variant="striped" size="sm">
+        <Thead>
+          <Tr>
+            <Th>Gene</Th>
+            <Th>ClinVar LP/P and gnomAD LoF</Th>
+            <Th>Estimates available on GeniE</Th>
+            <Th>Supporting Documents</Th>
+            <Th>Contact</Th>
+            <Th>Notes</Th>
+            <Th>Prevalence Orphanet</Th>
+            <Th>Prevalence GeneReviews</Th>
+            <Th>Prevalence Other</Th>
+            <Th>Incidence Other</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {dashboardList.map((dashboardRow: any) => {
+            return (
+              <Tr>
+                <Td>{dashboardRow.gene}</Td>
+                <Td>{dashboardRow.gnomad_lof}</Td>
+                <Td>
+                  <Link
+                    as={RRLink}
+                    to={`/variantlist/${dashboardRow.genie_link}`}
+                  >
+                    {dashboardRow.genie_estimates}
+                  </Link>
+                </Td>
+                <Td>
+                  <SupportingDocuments
+                    document_exists={dashboardRow.is_document}
+                  />
+                </Td>
+                <Td>{dashboardRow.contact}</Td>
+                <Td>{dashboardRow.notes}</Td>
+                <Td>
+                  <Link
+                    as={RRLink}
+                    to={`/variantlist/${dashboardRow.prevalence_orph_link}`}
+                  >
+                    {dashboardRow.prevalence_orph}
+                  </Link>
+                </Td>
+                <Td>
+                  <Link
+                    as={RRLink}
+                    to={`/variantlist/${dashboardRow.prevalence_genereviews_link}`}
+                  >
+                    {dashboardRow.prevalence_genereviews}
+                  </Link>
+                </Td>
+                <Td>{dashboardRow.prevalence_other}</Td>
+                <Td>{dashboardRow.incidence_other}</Td>
+              </Tr>
+            );
+          })}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
+};
 
 const DashboardContainer = () => {
-  const temp_data = [
+  const mock_data = [
     {
-      gene: "PCSK9", gnomad_lof: "1/200000", genie_estimates: "1/250000", genie_link: "abcde", is_document: false, notes: "nothing", 
-      prevalence_orph: "1/150000", prevalence_orph_link: "aoisf", prevalence_genereviews: "1/1000000", prevalence_genereviews_link: "aosifo",
-      prevalence_other: "oaihs", incidence_other: "aosihfapi"
+      gene: "PCSK9",
+      gnomad_lof: "1/200000",
+      genie_estimates: "1/250000",
+      genie_link: "abcde",
+      is_document: false,
+      notes: "",
+      prevalence_orph: "1/150000",
+      prevalence_orph_link: "aoisf",
+      prevalence_genereviews: "1/1000000",
+      prevalence_genereviews_link: "aosifo",
+      prevalence_other: "oaihs",
+      incidence_other: "aosihfapi",
     },
     {
-      gene: "BRCA2", gnomad_lof: "1/300000", genie_estimates: "1/100000", genie_link: "sdoih", is_document: true, notes: "nothing", 
-      prevalence_orph: "1/140000", prevalence_orph_link: "oaish", prevalence_genereviews: "1/1200000", prevalence_genereviews_link: "awoiehg",
-      prevalence_other: "oaihs", incidence_other: "aosihfapi"
-    }
-  ]
-  const [data, setData] = useState([]);
+      gene: "BRCA2",
+      gnomad_lof: "1/300000",
+      genie_estimates: "1/100000",
+      genie_link: "sdoih",
+      is_document: true,
+      notes: "",
+      prevalence_orph: "1/140000",
+      prevalence_orph_link: "oaish",
+      prevalence_genereviews: "1/1200000",
+      prevalence_genereviews_link: "awoiehg",
+      prevalence_other: "oaihs",
+      incidence_other: "aosihfapi",
+    },
+  ];
   const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState<any[]>([]);
   const [error, setError] = useState<Error | null>(null);
 
   useEffect(() => {
-
     setIsLoading(true);
-    //@ts-ignore
-    setData(temp_data);
 
-    //TO DO
+    // TODO: (rgrant) This call to variant lists does nothing, make this call
+    //   the new endpoint once that is created
+    get("/variant-lists/")
+      .then(() => setData(mock_data), setError)
+      .finally(() => {
+        setIsLoading(false);
+      });
 
-    // get("/variant-lists/", {
-    //   ordering: Array.isArray(orderBy) ? orderBy.join(",") : orderBy,
-    // })
-    //   .then((variantLists) => setVariantLists(variantLists), setError)
-    //   .finally(() => {
-    //     setIsLoading(false);
-    //   });
-
-    // TO DO
-    //setIsLoading(false);
-    
-
-  }, [temp_data]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   let content = null;
 
@@ -135,74 +183,49 @@ const DashboardContainer = () => {
       </Alert>
     );
   } else if (data.length === 0) {
-    content = (
-      <Text>
-        No prevalence lists have been created yet.{" "}
-      </Text>
-    );
+    content = <Text>There are no entries in the dashboard</Text>;
   } else {
-    content = (
-      <>
-        <TableContainer>
-        <Table variant='simple'>
-          <TableCaption>Imperial to metric conversion factors</TableCaption>
-          <Thead>
-            <Tr>
-              <Th>Gene</Th>
-              <Th>ClinVar LP/P and gnomAD LoF</Th>
-              <Th>Estimates available on GeniE</Th>
-              <Th>Supporting Documents</Th>
-              <Th>Contact</Th>
-              <Th>Notes</Th>
-              <Th>Prevalence Orphanet</Th>
-              <Th>Prevalence GeneReviews</Th>
-              <Th>Prevalence Other</Th>
-              <Th>Incidence Other</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {data.map(
-              (d: any) => {
-                return (
-                  <Tr>
-                    <Td>{d.gene}</Td>
-                    <Td>{d.gnomad_lof}</Td>
-                    <Td><Link as={RRLink} to={`/variantlist/${d.genie_link}`}>
-                      {d.genie_estimates}
-                    </Link></Td>
-                    <Td><SupportingDocuments 
-                    document_exists = {d.is_document}/></Td>
-                    <Td>{d.contact}</Td>
-                    <Td>{d.notes}</Td>
-                    <Td><Link as={RRLink} to={`/variantlist/${d.prevalence_orph_link}`}>
-                      {d.prevalence_orph}
-                    </Link></Td>
-                    <Td><Link as={RRLink} to={`/variantlist/${d.prevalence_genereviews_link}`}>
-                      {d.prevalence_genereviews}
-                    </Link></Td>
-                    <Td>{d.prevalence_other}</Td>
-                    <Td>{d.incidence_other}</Td>
-                  </Tr>
-                )
-              }
-
-            )}
-          </Tbody>
-        </Table>
-      </TableContainer>
-      </>
-    );
+    content = <DashboardList dashboardList={data} />;
   }
 
   return (
     <>
-      <h1>Dashboard Page</h1>
+      <Text mb={4}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Amet massa vitae
+        tortor condimentum lacinia. Elementum facilisis leo vel fringilla est
+        ullamcorper eget nulla. Platea dictumst vestibulum rhoncus est
+        pellentesque elit ullamcorper.
+      </Text>
       {content}
-
-
-
     </>
   );
 };
 
-export default DashboardContainer;
+const DashboardPage = () => {
+  return (
+    <>
+      <DocumentTitle title="Prevalence lists" />
+
+      <Box mb={2}>
+        <Breadcrumb>
+          <BreadcrumbItem>
+            <BreadcrumbLink as={RRLink} to="/">
+              Home
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbItem isCurrentPage>
+            <span>Prevalence Lists</span>
+          </BreadcrumbItem>
+        </Breadcrumb>
+      </Box>
+      <Heading as="h1" mb={4}>
+        Dashboard
+      </Heading>
+
+      <DashboardContainer />
+    </>
+  );
+};
+
+export default DashboardPage;
