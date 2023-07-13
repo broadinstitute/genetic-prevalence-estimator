ae92fb9e109bb543f64d18a6d5ff5b428d1a2c1f
Riley Grant <grantrileyh@gmail.com>
2023-07-13 09:02:34 -0500

    Add public variant list tests

--- calculator/tests/test_models.py
+++ calculator/tests/test_models.py
@@ -5,7 +5,12 @@ import pytest
 from django.contrib.auth import get_user_model
 from django.db import IntegrityError
 
-from calculator.models import VariantList, VariantListAccessPermission
+
+from calculator.models import (
+    VariantList,
+    VariantListAccessPermission,
+    PublicVariantList,
+)
 
 
 User = get_user_model()
@@ -149,4 +154,108 @@ class TestVariantListAccessPermission:
                 user=user,
                 variant_list=variant_list,
                 level=VariantListAccessPermission.Level.EDITOR,
+            )
+
+
+class TestPublicVariantList:
+    @pytest.mark.django_db
+    def test_deleting_variant_list_deletes_public_variant_list(self):
+        submitter = User.objects.create(username="submitter", is_staff=False)
+        reviewer = User.objects.create(username="reviewer", is_staff=True)
+
+        variant_list = VariantList.objects.create(
+            id=1,
+            label="List 1",
+            type=VariantList.Type.CUSTOM,
+            metadata={
+                "version": "1",
+                "reference_genome": "GRCh37",
+                "gnomad_version": "2.1.1",
+                "gene_id": "ENSG00000169174",
+                "gene_symbol": "PCSK9",
+            },
+            variants=["1-55516888-G-GA"],
+        )
+
+        PublicVariantList.objects.create(
+            variant_list=variant_list,
+            submitted_by=submitter,
+            reviewed_by=reviewer,
+            public_status=PublicVariantList.PublicStatus.REJECTED,
+        )
+
+        assert PublicVariantList.objects.count() == 1
+        variant_list.delete()
+        assert PublicVariantList.objects.count() == 0
+
+    @pytest.mark.django_db
+    def test_duplicate_genes_lists_cannot_be_submitted(self):
+        submitter = User.objects.create(username="submitter", is_staff=False)
+        reviewer = User.objects.create(username="reviewer", is_staff=True)
+
+        list1 = VariantList.objects.create(
+            id=1,
+            label="List 1",
+            type=VariantList.Type.CUSTOM,
+            metadata={
+                "version": "1",
+                "reference_genome": "GRCh37",
+                "gnomad_version": "2.1.1",
+                "gene_id": "ENSG00000169174",
+                "gene_symbol": "PCSK9",
+            },
+            variants=["1-55516888-G-GA"],
+        )
+
+        # A public list can be created if there are no existing approved lists with the same gene_id
+        PublicVariantList.objects.create(
+            variant_list=list1,
+            submitted_by=submitter,
+            reviewed_by=reviewer,
+            public_status=PublicVariantList.PublicStatus.REJECTED,
+        )
+
+        list2 = VariantList.objects.create(
+            id=2,
+            label="List 2",
+            type=VariantList.Type.CUSTOM,
+            metadata={
+                "version": "1",
+                "reference_genome": "GRCh37",
+                "gnomad_version": "2.1.1",
+                "gene_id": "ENSG00000169174",
+                "gene_symbol": "PCSK9",
+            },
+            variants=["1-55516888-G-GA"],
+        )
+
+        # A public list can still be created if there's an existing list with a the same gene_id that is not approved
+        PublicVariantList.objects.create(
+            variant_list=list2,
+            submitted_by=submitter,
+            reviewed_by=reviewer,
+            public_status=PublicVariantList.PublicStatus.APPROVED,
+        )
+
+        list3 = VariantList.objects.create(
+            id=3,
+            label="List 3",
+            type=VariantList.Type.CUSTOM,
+            metadata={
+                "version": "1",
+                "reference_genome": "GRCh37",
+                "gnomad_version": "2.1.1",
+                "gene_id": "ENSG00000169174",
+                "gene_symbol": "PCSK9",
+            },
+            variants=["1-55516888-G-GA"],
+        )
+
+        with pytest.raises(IntegrityError):
+            # A public list cannot be created if there's an existing approved list with the same gene_id
+            PublicVariantList.objects.create(
+                variant_list=list3,
+                submitted_by=submitter,
+                reviewed_by=reviewer,
+                public_status=PublicVariantList.PublicStatus.PENDING,
             )
