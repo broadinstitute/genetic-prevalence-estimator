41ebc277dfa7db95b73a25ce63cc7112db091f4c
Riley Grant <grantrileyh@gmail.com>
2023-11-24 13:01:03 -0500

    Update accepted gnomad versions in calculator

--- calculator/src/calculator/constants.py
+++ calculator/src/calculator/constants.py
@@ -1,10 +1,12 @@
 REFERENCE_GENOMES = ["GRCh37", "GRCh38"]
 
 
-GNOMAD_VERSIONS = ["2.1.1", "3.1.2"]
+GNOMAD_VERSIONS = ["2.1.1", "3.1.2", "4.0.0", "4.0.0_non-ukb"]
 
 
 GNOMAD_REFERENCE_GENOMES = {
     "2.1.1": "GRCh37",
     "3.1.2": "GRCh38",
+    "4.0.0": "GRCh38",
+    "4.0.0_non-ukb": "GRCh38",
 }
