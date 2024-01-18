3f80c45376d90a586f5e9273aa745947cd850b8e
Riley Grant <grantrileyh@gmail.com>
2024-01-18 10:41:44 -0600

    Remove X-Robots-Tag http header

--- website/src/website/middleware.py
+++
@@ -1,8 +0,0 @@
-class RobotsMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        response = self.get_response(request)
-        response["X-Robots-Tag"] = "all"
-        return response
--- website/src/website/settings/base.py
+++ website/src/website/settings/base.py
@@ -55,7 +55,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "website.middleware.RobotsMiddleware",
 ]
 
 AUTHENTICATION_BACKENDS = [
