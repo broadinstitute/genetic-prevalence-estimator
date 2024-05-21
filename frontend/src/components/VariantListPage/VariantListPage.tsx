e3c229023ed14337cef42857ad89197e0fc1a9d7
Riley Grant <grantrileyh@gmail.com>
2024-05-21 16:27:17 -0500

    refactor(frontend): split SV id predicate into its own file

--- frontend/src/components/VariantListPage/VariantListPage.tsx
+++ frontend/src/components/VariantListPage/VariantListPage.tsx
@@ -61,11 +61,7 @@ import {
   VariantListCalculations,
 } from "./VariantListCalculations/calculations";
 import VariantListCharts from "./VariantListCalculations/VariantListCharts";
-
-export const isStructuralVariantId = (id: string) => {
-  const svRegex = /^(BND|CPX|CTX|DEL|DUP|INS|INV|CNV)_CHR((1[0-9]|2[0-2]|[1-9])|X|Y)_([0-9a-f]*)$/i;
-  return svRegex.test(id);
-};
+import { isStructuralVariantId } from "../identifiers";
 
 const addVariantsToVariantList = (
   uuid: string,
