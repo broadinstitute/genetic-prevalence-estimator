dbfa666896e13a9c37a302909a9a2e8ce0adebfd
Elissa Alarmani <ealarman@broadinstitute.org>
2025-05-16 10:20:46 -0400

    feat(frontend): add dominant genes to dashboard page

--- frontend/src/components/DashboardListPage/DashboardListsPage.tsx
+++ frontend/src/components/DashboardListPage/DashboardListsPage.tsx
@@ -45,7 +45,11 @@ import { VariantList } from "../../types";
 import ButtonWithConfirmation from "../ButtonWithConfirmation";
 import DocumentTitle from "../DocumentTitle";
 
-import { renderFrequencyFraction } from "../VariantListPage/VariantListCalculations/calculationsDisplayFormats";
+import {
+  renderFrequencyFraction,
+  calculateFrequencyFractionOver100000,
+  renderFrequencyFractionOver100000,
+} from "../VariantListPage/VariantListCalculations/calculationsDisplayFormats";
 
 type DashboardList = {
   gene_id: string;
@@ -58,6 +62,13 @@ type DashboardList = {
   };
   estimates: {
     genetic_prevalence: number[];
+  };
+  variant_calculations: {
+    carrier_frequency: number[];
+    carrier_frequency_raw_numbers: number[];
+    carrier_frequency_simplified: number[];
+    prevalence: number[];
+    prevalence_bayesian: number[];
   };
   genetic_prevalence_orphanet: string;
   genetic_prevalence_genereviews: string;
@@ -72,6 +83,29 @@ type DashboardList = {
       };
     };
     owners: string[];
+  };
+  dominant_dashboard_list?: {
+    date_created: string;
+    metadata: {
+      gene_id: string;
+      gene_symbol: string;
+      transcript_id: string;
+      gnomad_version: string;
+    };
+    de_novo_variant_calculations: {
+      missense_de_novo_incidence: number;
+      lof_de_novo_incidence: number;
+      total_de_novo_incidence: number;
+      inputs: {
+        oe_mis_capped: number;
+        mu_mis: number;
+        oe_lof_capped: number;
+        mu_lof: number;
+        oe_mis_prior: number;
+        oe_lof_prior: number;
+      };
+    };
+    inheritance_type: string;
   };
   inheritance_type: string;
 };
@@ -163,24 +197,120 @@ const BASE_COLUMNS: ColumnDef[] = [
   },
 
   {
+    key: "inheritance_type",
+    heading: "Mode of Inheritance",
+    width: 200,
+    sortKey: (dashboardList) => {
+      {
+        console.log(dashboardList);
+      }
+      return dashboardList.inheritance_type;
+    },
+    render: (dashboardList) => {
+      return <Cell maxWidth={130}>{dashboardList.inheritance_type}</Cell>;
+    },
+  },
+
+  {
+    key: "aggregate_allele_freq_lp_p",
+    heading: "Aggregate allele frequency for LP/P variants",
+    width: 200,
+    sortKey: (dashboardList) => {
+      const carrierFreq =
+        dashboardList?.variant_calculations?.carrier_frequency;
+      return carrierFreq && carrierFreq.length > 0
+        ? Math.round(1 / (carrierFreq[0] / 2))
+        : 0;
+    },
+    render: (dashboardList) => {
+      const carrierFreq =
+        dashboardList?.variant_calculations?.carrier_frequency;
+      return carrierFreq && carrierFreq.length > 0 ? (
+        <Cell maxWidth={130}>
+          {renderFrequencyFraction(carrierFreq[0] / 2)}
+        </Cell>
+      ) : null;
+    },
+  },
+
+  {
+    key: "est_heterozygous_freq",
+    heading: "Estimated heterozygous frequency (carrier frequency)",
+    width: 200,
+
+    sortKey: (dashboardList) => {
+      const cf = dashboardList?.variant_calculations?.carrier_frequency?.[0];
+      return cf && !isNaN(cf) && cf !== 0 ? Math.round(1 / cf) : 0;
+    },
+
+    render: (dashboardList) => {
+      const cf = dashboardList?.variant_calculations?.carrier_frequency?.[0];
+      const isValid = typeof cf === "number" && !isNaN(cf) && cf > 0;
+
+      return isValid ? (
+        <Cell maxWidth={130}>{renderFrequencyFraction(cf)}</Cell>
+      ) : null;
+    },
+  },
+
+  {
     key: "dashboard_estimate",
     heading:
-      "Preliminary genetic prevalence estimates (ClinVar LP/P and gnomAD HC LoF)",
+      "Estimated biallelic frequency (Preliminary genetic prevalence) - AR ONLY",
     headingTooltip:
       "Preliminary genetic prevalence estimates are algorithmically generated using ClinVar pathogenic/likely pathogenic variants and gnomAD high confidence predicted loss-of-function variants only. These estimates have not been manually reviewed and may contain non-disease causing variants. Use with caution.",
     width: 200,
     sortKey: (dashboardList) => {
-      return dashboardList.estimates.genetic_prevalence[0] !== 0
-        ? Math.round(1 / dashboardList.estimates.genetic_prevalence[0])
-        : 0;
+      const gp = dashboardList?.estimates?.genetic_prevalence?.[0];
+      return gp && !isNaN(gp) && gp !== 0 ? Math.round(1 / gp) : 0;
     },
     render: (dashboardList) => {
-      return (
+      const gp = dashboardList?.estimates?.genetic_prevalence?.[0];
+      const isValid = typeof gp === "number" && !isNaN(gp) && gp > 0;
+
+      if (dashboardList?.inheritance_type === "AD") {
+        return <Cell maxWidth={200}>N/A - Dominant disease</Cell>;
+      }
+
+      return isValid ? (
         <Cell maxWidth={200}>
           <Link as={RRLink} to={`/dashboard/${dashboardList.gene_id}`}>
-            {renderFrequencyFraction(
-              dashboardList.estimates.genetic_prevalence[0]
-            )}
+            {renderFrequencyFraction(gp)}
+          </Link>
+        </Cell>
+      ) : null;
+    },
+  },
+
+  {
+    key: "de_novo_dashboard_estimate",
+    heading: "Estimated incidence of de novo variation (per 100,000)",
+    // TODO: UPDATE TOOLTIPS
+    headingTooltip: "Estimated incidence of de novo variation (per 100,000)",
+    width: 200,
+    sortKey: (dashboardList) => {
+      const incidence =
+        dashboardList.dominant_dashboard_list?.de_novo_variant_calculations
+          ?.total_de_novo_incidence;
+
+      return incidence ?? 0;
+    },
+    render: (dashboardList) => {
+      const incidence =
+        dashboardList.dominant_dashboard_list?.de_novo_variant_calculations
+          ?.total_de_novo_incidence;
+
+      if (incidence === undefined || incidence === null) return null;
+
+      const per100k = (incidence * 100_000).toFixed(3);
+
+      return (
+        <Cell maxWidth={200}>
+          <Link
+            as={RRLink}
+            to={`/dashboard-incidence/${dashboardList.gene_id}`}
+          >
+            {per100k}
           </Link>
         </Cell>
       );
@@ -189,7 +319,7 @@ const BASE_COLUMNS: ColumnDef[] = [
 
   {
     key: "representative_estimate",
-    heading: "Estimates available on GeniE",
+    heading: "Curated Estimates Public on GeniE",
     width: 200,
     sortKey: (dashboardList) => {
       if (dashboardList.representative_variant_list) {
@@ -418,7 +548,8 @@ const DataRow = ({
 const DashboardLists = (props: {
   dashboardListsStore: Store<DashboardList[]>;
 }) => {
-  const dashboardLists = useStore(props.dashboardListsStore);
+  const { dashboardListsStore } = props;
+  const dashboardLists = useStore(dashboardListsStore);
 
   type Filter = {
     searchText: string;
@@ -516,6 +647,43 @@ const DashboardLists = (props: {
       (error) => {
         toast({
           title: "Unable to load dashboard lists",
+          description: renderErrorDescription(error),
+          status: "error",
+          duration: 10_000,
+          isClosable: true,
+        });
+      }
+    );
+  };
+
+  const loadDominantDashboardLists = (): Promise<void> => {
+    if (!file) {
+      toast({
+        title: "You need to add a file",
+        status: "error",
+        duration: 10_000,
+        isClosable: true,
+      });
+      return new Promise<void>((resolve, reject) => resolve());
+    }
+
+    const formData = new FormData();
+    const blob = new Blob([file!], { type: file!.type });
+    formData.append("csv_file", blob, file!.name);
+
+    return postFile(`/dominant-dashboard-lists/load`, formData).then(
+      () => {
+        toast({
+          title: "Dominant Dashboard lists loaded!",
+          status: "success",
+          duration: 30_000,
+          isClosable: true,
+        });
+        setFile(null);
+      },
+      (error) => {
+        toast({
+          title: "Unable to load dominant dashboard lists",
           description: renderErrorDescription(error),
           status: "error",
           duration: 10_000,
@@ -703,6 +871,51 @@ const DashboardLists = (props: {
                     confirmButtonText="Re-load"
                     onClick={() => {
                       loadDashboardLists();
+                    }}
+                  >
+                    Load
+                  </ButtonWithConfirmation>
+                </Box>
+              </AccordionPanel>
+            </AccordionItem>
+          </Accordion>
+          <Accordion allowToggle>
+            <AccordionItem>
+              <h2>
+                <AccordionButton>
+                  <Box as="span" flex="1" textAlign="left">
+                    Upload dominant dashboard lists from .csv file
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4}>
+                <Box mb={4}>
+                  <Input
+                    type="file"
+                    onChange={handleFileChange}
+                    placeholder="Add a file populate lists"
+                    size="md"
+                    mb={2}
+                    sx={{
+                      "::file-selector-button": {
+                        height: 10,
+                        padding: 0,
+                        mr: 4,
+                        background: "none",
+                        border: "none",
+                        fontWeight: "bold",
+                      },
+                    }}
+                  />
+
+                  <ButtonWithConfirmation
+                    size="sm"
+                    colorScheme="blue"
+                    confirmationPrompt="This cannot be undone."
+                    confirmButtonText="Re-load"
+                    onClick={() => {
+                      loadDominantDashboardLists();
                     }}
                   >
                     Load
