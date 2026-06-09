e880191cef111dca6be1124f1896a5a22956e729
Riley Grant <grantrileyh@gmail.com>
2026-06-08 19:10:43 -0500

    feat(pipelines): combine has option to generate 2 download files

--- data-pipelines/combine_dashboard_downloads.py
+++ data-pipelines/combine_dashboard_downloads.py
@@ -1,8 +1,8 @@
 import os
 import pandas as pd
-import re
 from datetime import datetime
 import argparse
+import json
 
 
 def combine_csv_files(output_directory, output_filename):
@@ -41,20 +41,89 @@ def combine_csv_files(output_directory, output_filename):
             print(f"Error reading {filename}, error: {e}. Skipping.")
 
     combined_df = pd.concat(df_full, ignore_index=True)
+
+    combined_df = combined_df.drop(
+        columns=[
+            "oe_missense_prior",
+            "oe_missense_gene",
+            "MU_mis",
+            "oe_lof_prior",
+            "oe_lof_gene",
+            "MU_lof",
+            "Estimated incidence of de novo variation",
+            "Estimated incidence of de novo variation (per 100,000)",
+        ]
+    )
+
     output_filepath = os.path.join(recessive_downloads_directory, output_filename)
     combined_df.to_csv(output_filepath, index=False)
 
     print(f"Successfully wrote combined CSV to {output_filepath}")
 
 
-def create_5k_downloads(_input_recessive_file, _input_dominant_file, _output_filename):
-    print("Running create 5k downloads helper")
-    pass
+def create_joined_downloads(
+    output_directory, input_recessive_downloads_file, input_dominant_models_file
+):
+    print("Running create joined downloads helper")
 
+    df_recessive = pd.read_csv(input_recessive_downloads_file)
 
-def create_17k_downloads(_input_recessive_file, _input_dominant_file, _output_filename):
-    print("Running create 17k downloads helper")
-    pass
+    # ---
+
+    df_dominant = pd.read_csv(input_dominant_models_file)
+
+    # flatten dominant calcs
+    df_dominant = df_dominant.join(
+        df_dominant["de_novo_variant_calculations"].apply(json.loads).apply(pd.Series)
+    )
+    df_dominant = df_dominant.join(df_dominant["inputs"].apply(pd.Series))
+    df_dominant["de_novo_estimated_per_100k"] = (
+        pd.to_numeric(df_dominant["total_de_novo_incidence"], errors="coerce")
+        .mul(100_000)
+        .fillna("")
+    )
+
+    rename_dictionary = {
+        "gene_symbol": "gene_symbol",
+        "gene_id": "gene_id",
+        "oe_mis_prior": "oe_missense_prior",
+        "oe_mis": "oe_missense_gene",
+        "mu_mis": "MU_mis",
+        "oe_lof_prior": "oe_lof_prior",
+        "oe_lof": "oe_lof_gene",
+        "mu_lof": "MU_lof",
+        "total_de_novo_incidence": "Estimated incidence of de novo variation",
+        "de_novo_estimated_per_100k": "Estimated incidence of de novo variation (per 100,000)",
+    }
+
+    df_dominant = df_dominant.rename(columns=rename_dictionary)[
+        [*rename_dictionary.values()]
+    ]
+
+    df_5k_merged = df_recessive.merge(df_dominant, on="gene_symbol", how="left")
+    output_filepath = os.path.join(output_directory, "dashboard-summary.csv")
+    df_5k_merged.to_csv(output_filepath, index=False)
+    print("Wrote dashboard summary (~5k disease associated genes)")
+
+    df_17k_merged = df_dominant.merge(df_recessive, on="gene_symbol", how="left")
+
+    # re-order so that dominant columns are always at the end
+    dominant_columns = [
+        "oe_missense_prior",
+        "oe_missense_gene",
+        "MU_mis",
+        "oe_lof_prior",
+        "oe_lof_gene",
+        "MU_lof",
+        "Estimated incidence of de novo variation",
+        "Estimated incidence of de novo variation (per 100,000)",
+    ]
+    recessive_columns = [c for c in df_17k_merged.columns if c not in dominant_columns]
+    df_17k_merged = df_17k_merged[recessive_columns + dominant_columns]
+
+    output_filepath = os.path.join(output_directory, "dashboard-all-genes-summary.csv")
+    df_17k_merged.to_csv(output_filepath, index=False)
+    print("Wrote dashboard summary (~17k all constraint genes no olfactory)")
 
 
 # e.g.:
@@ -64,9 +133,9 @@ def create_17k_downloads(_input_recessive_file, _input_dominant_file, _output_fi
 #     --output-filename combined-recessive-downloads.csv
 
 # uv run python data-pipelines/combine_dashboard_downloads.py \
-#     --action create-5k-download \
-#     --input-recessive-file TK-FILE-HERE \
-#     --input-dominant-file TK-FILE-HERE \
+#     --action create-joined-downloads \
+#     --input-recessive-downloads-file /Users/rgrant/dev/work-broad/genetic-prevalence-estimator/data/output/recessive_dashboard/downloads/combined-recessive-downloads-no-mt.csv \
+#     --input-dominant-models-file /Users/rgrant/dev/work-broad/genetic-prevalence-estimator/data/output/dominant_dashboard/dominant-dashboard-download.csv \
 #     --output-filename disease-associated-genes-download-file.csv
 
 # uv run python data-pipelines/combine_dashboard_downloads.py \
@@ -85,40 +154,33 @@ def main():
         "--action",
         choices=[
             "merge-recessive-downloads",
-            "create-5k-download",
-            "create-17k-download",
+            "create-joined-downloads",
+            # "create-5k-download",
+            # "create-17k-download",
         ],
         required=True,
     )
     parser.add_argument("--output-filename", required=False)
-    parser.add_argument("--input-recessive-file", required=False)
-    parser.add_argument("--input-dominant-file", required=False)
+    parser.add_argument("--input-recessive-downloads-file", required=False)
+    parser.add_argument("--input-dominant-models-file", required=False)
 
     args = parser.parse_args()
 
     print(f"Running {args.action} ...")
 
     default_output_filename = None
-    input_recessive_file = args.input_recessive_file
-    input_dominant_file = args.input_dominant_file
-    both_inputs_defined = input_recessive_file and input_dominant_file
+    input_recessive_downloads_file = args.input_recessive_downloads_file
+    input_dominant_models_file = args.input_dominant_models_file
+    both_inputs_defined = input_recessive_downloads_file and input_dominant_models_file
 
     if args.action == "merge-recessive-downloads":
         default_output_filename = "download_single-file-recessive-combined.csv"
 
-    elif args.action == "create-5k-download":
+    elif args.action == "create-joined-downloads":
         default_output_filename = "download_5k-condition-associated-genes.csv"
         if not both_inputs_defined:
             print(
-                f"Error! For creating 5k download, both recessive and dominant inputs must be defined"
-            )
-            exit(0)
-
-    elif args.action == "create-17k-download":
-        default_output_filename = "download_17k-all-genes.csv"
-        if not both_inputs_defined:
-            print(
-                f"Error! For creating 5k download, both recessive and dominant inputs must be defined"
+                f"Error! For creating joined downloads, both recessive and dominant inputs must be defined"
             )
             exit(0)
 
@@ -139,11 +201,10 @@ def main():
     if args.action == "merge-recessive-downloads":
         combine_csv_files(output_directory, output_filename)
 
-    elif args.action == "create-5k-downloads":
-        create_5k_downloads(input_recessive_file, input_dominant_file, output_filename)
-
-    elif args.action == "create-17k-downloads":
-        create_17k_downloads(input_recessive_file, input_dominant_file, output_filename)
+    elif args.action == "create-joined-downloads":
+        create_joined_downloads(
+            output_directory, input_recessive_downloads_file, input_dominant_models_file
+        )
 
     else:
         print(f"Error! Unrecognized action: action: '{args.action}'")
